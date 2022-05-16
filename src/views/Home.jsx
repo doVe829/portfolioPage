776b6872fe628f19e92d671e909897964a292d59
doVe829 <dvennegeerts@gmail.com>
2022-05-16 17:44:50 +0200

    changes title, adds element and style

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,23 +2,35 @@ import '../styles/Home.scss';
 import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const [showTest, setShowTest] = useState('web');
-  const wordList = ['the web.', 'the desktop.', 'the moon?', 'the phone.'];
-  const word = () => {
-    return wordList[Math.floor(Math.random() * wordList.length)];
+  const [platform, setPlatform] = useState('web');
+  const platformList = [
+    'the web.',
+    'the desktop.',
+    'the moon?',
+    'mobile.',
+    'myself.',
+  ];
+  const randomPlatform = () => {
+    return platformList[Math.floor(Math.random() * platformList.length)];
   };
   useEffect(() => {
     const interval = setInterval(() => {
-      setShowTest(word);
+      setPlatform(randomPlatform);
     }, 2000);
     return () => clearInterval(interval);
   }, []);
   return (
     <section className="home">
       <h1>Hi, my name is</h1>
-      <h2 className="accent-color">doVe</h2>
+      <div className="row">
+        <h2 className="accentColor">doVe</h2>
+        <sup>(well, not really.)</sup>
+      </div>
       {/* ADD ANIMATION */}
-      <h3>I sometimes build stuff for {showTest}</h3>
+      <h3>
+        I sometimes build stuff for{' '}
+        <span className="accentColor platformText">{platform}</span>
+      </h3>
       <p className="col-sm-3">
         I'm a web developer based in germany, that is currently working on great
         tourism experiences at WORKNAME
